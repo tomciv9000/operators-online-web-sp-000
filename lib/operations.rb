a51d8f2b18393553d514623563bf67b1d541499e
Tom White <eugene.white@gmail.com>
2019-05-01 04:47:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
-def unsafe?(speed)
+require 'pry' 
 
+def unsafe?(speed)
+  if speed > 60 || speed < 40
+    return true 
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
+
+binding.pry
 	
 
 
